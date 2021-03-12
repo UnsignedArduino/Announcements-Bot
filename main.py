ac00bcd5b005f4ecb2e7c648e1a473bc00ff2ce8
UnsignedArduino <UnsignedArduino@outlook.com>
2021-03-12 12:54:41 -0500

    Make tasks.loop function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import logging
 from create_logger import create_logger
@@ -15,6 +15,13 @@ bot = commands.Bot(command_prefix="~")
 @bot.event
 async def on_ready():
     logger.info(f"{bot.user} has connected to {len(bot.guilds)} guild(s)!")
+    check_to_send.start()
+
+
+@tasks.loop(minutes=1)
+async def check_to_send():
+    logger.debug(f"Checking for messages to send...")
+    logger.debug(f"Finished!")
 
 
 bot.run(TOKEN)
