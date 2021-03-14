0518539cea9bf0a750c1b1d8ac7c4e7521730791
UnsignedArduino <UnsignedArduino@outlook.com>
2021-03-14 12:08:31 -0400

    Enable file logging

--- create_logger.py
+++ create_logger.py
@@ -48,15 +48,15 @@ def create_logger(name: str = __name__, level: int = logging.DEBUG) -> logging.g
     console_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     console_handler.setFormatter(fmt=console_formatter)
     console_handler.setLevel(level=level)
-    # file_handler = logging.FileHandler(filename=LOG_LOCATION)
-    # file_handler.setLevel(level=level)
-    # file_handler.setFormatter(fmt=console_formatter)
-    # file_handler.setLevel(level=level)
+    file_handler = logging.FileHandler(filename=LOG_LOCATION)
+    file_handler.setLevel(level=level)
+    file_handler.setFormatter(fmt=console_formatter)
+    file_handler.setLevel(level=level)
     logger.propagate = False
     if console_handler not in logger.handlers:
         logger.addHandler(hdlr=console_handler)
-    # if file_handler not in logger.handlers:
-    #     logger.addHandler(hdlr=file_handler)
+    if file_handler not in logger.handlers:
+        logger.addHandler(hdlr=file_handler)
     logger.setLevel(level=level)
     logger.debug(f"Created logger named {repr(name)} with level {repr(level)}")
     logger.debug(f"Handlers for {repr(name)}: {repr(logger.handlers)}")
